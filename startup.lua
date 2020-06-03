1a854091d37c922aceacfff9cd32204835a752a4
infokun <66201147+Info-kun@users.noreply.github.com>
2020-06-03 11:05:38 +0300

    Updated Startup.lua

--- startup.lua
+++ startup.lua
@@ -1,1 +1,11 @@
+os.pullEvent=os.pullEventRaw
+if not(fs.exists('/system/main.lua')) then
+  while true do
+    print('Unable to locate "/system/main.lua"')
+    os.sleep(2)
+    os.reboot()
+  end
+else
+  shell.run('/system/main.lua')
+end
 
